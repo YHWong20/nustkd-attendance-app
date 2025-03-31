b6b5be180f386cc866d05f261d928cb8ceacc513
YHWong20 <yihao.wong@u.nus.edu>
2025-03-31 21:11:17 +0800

    Update hostname

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     allowedHosts: [
-      "nustkd-attendance-app-e897e90f665b.herokuapp.com/"
+      "nustkd-attendance-app-e897e90f665b.herokuapp.com"
     ]
   },
   plugins: [svelte()],
